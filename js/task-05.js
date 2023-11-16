ad0ec21b08d7abe6a9f453ab159aadfe1639fc4d
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2023-11-16 08:35:56 -0800

    Comments added

--- js/task-05.js
+++ js/task-05.js
@@ -2,6 +2,9 @@ const inputName = document.querySelector('#name-input');
 const outputName = document.querySelector('#name-output');
 
 inputName.addEventListener('input', event => {
+  //  Тут умовою тринарного опература являється вираз:
+  // event.currentTarget.value.length
+  // який або 0(false) або більше нуля(true)
   outputName.textContent = event.currentTarget.value.length
     ? event.currentTarget.value
     : 'Anonymous';
