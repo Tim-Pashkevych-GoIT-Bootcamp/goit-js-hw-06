8239eb6ffcffe9a97e9f2f36be90654a7c16d162
Tymofii Pashkevych <tymofii.pashkevych@gmail.com>
2023-11-16 08:27:38 -0800

    Task 4 fixed

--- js/task-04.js
+++ js/task-04.js
@@ -4,16 +4,12 @@ const buttons = document.querySelectorAll('button');
 
 buttons.forEach(button => {
   button.addEventListener('click', event => {
-    event.preventDefault();
-
     if (event.currentTarget.dataset.action === 'decrement') {
       counterValue--;
     } else {
       counterValue++;
     }
 
-    if (counter) {
-      counter.textContent = counterValue;
-    }
+    counter.textContent = counterValue;
   });
 });
